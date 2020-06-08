54227f4042733018f70e7e000d78748e4cef5cfe
Julien Flusin <julienflusin3@gmail.com>
2020-06-08 04:11:18 +0200

    Update config.php

--- webroot/download/config.php
+++ webroot/download/config.php
@@ -8,3 +8,4 @@ date_default_timezone_set('Europe/Paris');
 
 $clientInfo = $_SERVER['REMOTE_ADDR'] . " - [" . date('j/M/Y:G:i:s T')  . "] - '" . $_SERVER['HTTP_USER_AGENT'] . "' - " . $_SERVER['HTTP_REFERER'] . "\n"; 
 ?>
+.
