f0ac8b6dea30d0771f2efbc7aed448b24f6fa085
Julien Flusin <julienflusin3@gmail.com>
2020-06-08 03:35:41 +0200

    Create index.php

---
+++ webroot/download/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once('config.php');
+
+if (isset($_REQUEST['file'])) {
+	$filename = $_REQUEST['file'];
+	$filepath = $filefolder . $filename;
+	$statspath = $statsfolder . $filename . ".log";
+	if (file_exists($filepath)) {
+		@file_put_contents($statspath, $clientInfo, FILE_APPEND);
+		header("Location: " . $filepath);
+	}
+}
+exit('Bad request');
+?>
