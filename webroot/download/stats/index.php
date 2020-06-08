531d7fcb21277fdb491b8b7477c70c9f43dd1186
Julien Flusin <julienflusin3@gmail.com>
2020-06-08 03:36:20 +0200

    Create index.php

---
+++ webroot/download/stats/index.php
@@ -0,0 +1,24 @@
+<?php
+require_once('../config.php');
+$files = scandir(".");
+
+foreach ($files as $file) {
+	if (substr($file, -4) === ".log") {
+		if ($nixWithExec) {
+			$totalLines = intval(exec('wc -l ' . $file));
+		} else {
+			$totalLines = 0;
+			$handle = fopen($file, "r");
+			while(!feof($handle)){
+			  $line = fgets($handle);
+			  $totalLines++;
+			}
+			fclose($handle);
+			$totalLines = $totalLines - 1; //This method counts the last empty line
+		}
+		print substr($file, 0, -4) . ": <a href=$file>" . $totalLines . "</a>";
+		print "<br />";
+
+	}
+}
+?>
