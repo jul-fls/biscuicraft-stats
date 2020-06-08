88fe50b69cb86e92f34a044fc13d0f72f92a4c47
Julien Flusin <julienflusin3@gmail.com>
2020-06-08 04:12:13 +0200

    Fichier Php de configuration du système de statistiques de téléchargements

--- webroot/download/config.php
+++ webroot/download/config.php
@@ -8,4 +8,3 @@ date_default_timezone_set('Europe/Paris');
 
 $clientInfo = $_SERVER['REMOTE_ADDR'] . " - [" . date('j/M/Y:G:i:s T')  . "] - '" . $_SERVER['HTTP_USER_AGENT'] . "' - " . $_SERVER['HTTP_REFERER'] . "\n"; 
 ?>
-.
