c5ee511b627c73afdbfe8cb5dc376edb7b86de5a
Julien Flusin <julienflusin3@gmail.com>
2020-06-08 03:35:12 +0200

    Create config.php

---
+++ webroot/download/config.php
@@ -0,0 +1,10 @@
+<?php
+$filefolder = 'files/';
+$statsfolder = 'stats/';
+
+$nixWithExec = true;
+
+date_default_timezone_set('Europe/Paris');
+
+$clientInfo = $_SERVER['REMOTE_ADDR'] . " - [" . date('j/M/Y:G:i:s T')  . "] - '" . $_SERVER['HTTP_USER_AGENT'] . "' - " . $_SERVER['HTTP_REFERER'] . "\n"; 
+?>
