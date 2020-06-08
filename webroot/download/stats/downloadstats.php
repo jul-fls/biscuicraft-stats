c9d6ecdc72df1f1e0daf7760fbe6fb4bb7118450
Julien Flusin <julienflusin3@gmail.com>
2020-06-08 03:36:44 +0200

    Create downloadstats.php

---
+++ webroot/download/stats/downloadstats.php
@@ -0,0 +1,7 @@
+<?php
+$Exefile = "Biscuicraft.exe.log";
+$totalExeLines = intval(exec('wc -l ' . $Exefile));
+$Jarfile = "Biscuicraft.jar.log";
+$totalJarLines = intval(exec('wc -l ' . $Jarfile));
+echo ($totalExeLines + $totalJarLines);
+?>
